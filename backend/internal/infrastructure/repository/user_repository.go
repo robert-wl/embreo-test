272f3061bac2f5d71bce16d71e85d40270d1a55b
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-12-21 15:19:29 +0700

    feat: added user repository

---
+++ backend/internal/infrastructure/repository/user_repository.go
@@ -0,0 +1,50 @@
+package repository
+
+import (
+	"github.com/robert-wl/backend/internal/domain/model"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	FindBySecureID(secureID string) (*model.User, error)
+	FindByUsername(email string) (*model.User, error)
+	Create(user *model.User) (*model.User, error)
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{
+		db: db,
+	}
+}
+
+func (r *userRepository) FindBySecureID(secureID string) (*model.User, error) {
+	var user model.User
+
+	if err := r.db.Where("secure_id = ?", secureID).First(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *userRepository) FindByUsername(username string) (*model.User, error) {
+	var user model.User
+
+	if err := r.db.Where("username = ?", username).First(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *userRepository) Create(user *model.User) (*model.User, error) {
+	if err := r.db.Create(user).Error; err != nil {
+		return nil, err
+	}
+
+	return r.FindBySecureID(user.SecureID)
+}
