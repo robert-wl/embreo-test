99367bb9a92aea70ad6099d4eadb1eeda845273d
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-12-21 09:36:08 +0700

    feat: added config for envs

---
+++ backend/config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"github.com/joho/godotenv"
+	"os"
+)
+
+type Config struct {
+	ServerPort string
+
+	PostgresUsername string
+	PostgresPassword string
+	PostgresHost     string
+	PostgresPort     string
+	PostgresDB       string
+}
+
+var config *Config
+
+func Get() *Config {
+	if config != nil {
+		return config
+	}
+
+	appEnv := getEnv("APP_ENV")
+	if err := godotenv.Load(); err != nil && appEnv != "production" {
+		panic("Error loading .env file: " + err.Error())
+	}
+
+	config = &Config{
+		ServerPort: getEnv("SERVER_PORT"),
+
+		PostgresUsername: getEnv("POSTGRES_USERNAME"),
+		PostgresPassword: getEnv("POSTGRES_PASSWORD"),
+		PostgresHost:     getEnv("POSTGRES_HOST"),
+		PostgresPort:     getEnv("POSTGRES_PORT"),
+		PostgresDB:       getEnv("POSTGRES_DB"),
+	}
+
+	return config
+}
+func getEnv(key string) string {
+	v := os.Getenv(key)
+
+	if v == "" {
+		panic("missing env var: " + key)
+	}
+
+	return v
+}
