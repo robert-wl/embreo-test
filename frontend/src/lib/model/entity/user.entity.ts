5dffcec6f3eb0721d4ecfa3665c1e9b3f47a2407
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-12-23 08:02:51 +0700

    feat: updated .env.example

--- frontend/src/lib/model/entity/user.entity.ts
+++ frontend/src/lib/model/entity/user.entity.ts
@@ -4,10 +4,16 @@ import { VendorEntity } from "@/lib/model/entity/vendor.entity.ts";
 export type UserEntity = {
   id: string;
   username: string;
-  role: string;
+  role: Role;
   created_at: string;
   updated_at: string;
 
   vendor?: VendorEntity;
   company?: CompanyEntity;
 };
+
+
+export enum Role {
+    VENDOR = "vendor",
+    COMPANY = "company",
+}
