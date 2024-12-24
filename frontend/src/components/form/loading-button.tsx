b7192fc63fc194d0722badef3dc0fd776cc86efe
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-12-24 12:05:23 +0700

    feat: added loading-button

---
+++ frontend/src/components/form/loading-button.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button.tsx";
+import { ComponentProps } from "react";
+import { Loader2 } from "lucide-react";
+
+interface Props extends ComponentProps<typeof Button> {
+  isLoading: boolean;
+  loadingText?: string;
+}
+
+export default function LoadingButton({ children, isLoading, loadingText = "Loading...", ...props }: Props) {
+  return (
+    <Button {...props}>
+      {isLoading ? (
+        <>
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" /> {loadingText}
+        </>
+      ) : (
+        <>{children}</>
+      )}
+    </Button>
+  );
+}
